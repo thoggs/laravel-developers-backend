26f5b1a08808c0afb022ceb188251fee4ed310d9
thoggs <thiagothogs@icloud.com>
2021-09-03 07:35:44 -0300

    update project configuration

--- production.sh
+++ production.sh
@@ -18,16 +18,12 @@ php artisan key:generate
 # clear Laravel cache
 php artisan route:clear
 php artisan config:clear
-php artisan view:clear
 
 # Optimizing Configuration Loading
 php artisan config:cache
 
 # Optimizing Route Loading
 php artisan route:cache
-
-# Optimizing View Loading
-php artisan view:cache
 
 php artisan migrate
 
